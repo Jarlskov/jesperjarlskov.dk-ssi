05739bd7aa34554d2a19af615dcd195330e84a15
Jesper Jarlskov <jesper@jarlskov.dk>
2025-11-12 22:29:03 +0100

    Remove dist folder and update config

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 // config.php
 
 return [
-    'baseUrl' => 'http://localhost:8000', // Change this to your actual domain when deploying
-    'siteTitle' => 'My Awesome Blog', // Your blog's title
-    'author' => 'Author Name', // Global author name
+    'baseUrl' => 'https://jesperjarlskov.dk/', // Change this to your actual domain when deploying
+    'siteTitle' => 'JesperJarlskov.dk', // Your blog's title
+    'author' => 'Jesper Jarlskov', // Global author name
     'defaultSocialImage' => '/assets/images/default-social.jpg', // Path relative to base URL
     'postsPerPage' => 5, // Number of posts to show per page on index (not implemented yet, but good to have)
 ];
