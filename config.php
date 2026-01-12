82fcb86b1ffedd86e457a287f3ccd95e9a073c7c
Jesper Jarlskov <jesper@jarlskov.dk>
2026-01-12 19:27:52 +0100

    Fix broken assets and links on GitHub Pages by using baseUrl in templates

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 // config.php
 
 return [
-    'baseUrl' => 'https://jesperjarlskov.dk/', // Change this to your actual domain when deploying
+    'baseUrl' => 'https://jarlskov.github.io/jesperjarlskov.dk-ssi/', // Change this to your actual domain when deploying
     'siteTitle' => 'JesperJarlskov.dk', // Your blog's title
     'author' => 'Jesper Jarlskov', // Global author name
     'defaultSocialImage' => '/assets/images/default-social.jpg', // Path relative to base URL
